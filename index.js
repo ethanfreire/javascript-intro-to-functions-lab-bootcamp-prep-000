4a853e503b660b0ef411b4f5c62f774706f4b205
Ethan Freire <element55star@gmail.com>
2019-02-25 08:15:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  var lowercase = string.toLowerCase
+  string.toLowerCase === lowercase
+  return ("I can't hear you!")
+}
